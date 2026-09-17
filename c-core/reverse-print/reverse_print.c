32ae239468961bc8142facce9ecda09d36f4d1ad
code-redwan <260278494+code-redwan@users.noreply.github.com>
2026-09-17 23:24:34 +0000

    Solve reverse-print
    Validated on Grademe on 2026-09-18.
    
    - reverse-print (practice)
    
    Every exercise here passed the Grademe grader: https://grademe.io
    
    Co-authored-by: Grademe <320286061+grademe-sync[bot]@users.noreply.github.com>

---
+++ c-core/reverse-print/reverse_print.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int		ft_strlen(char *str)
+{
+	int		len;
+
+	len = 0;
+	while (*(str + len))
+		len++;
+	return (len);
+}
+
+int	main(int argc, char **argv)
+{
+	int		len;
+
+	if (argc != 2)
+	{
+		write(1, "wrong number of arguments\n", 26);
+		return (0);
+	}
+	len = ft_strlen(argv[1]);
+	while (0 < len)
+	{
+		write(1, &argv[1][len - 1], 1);
+		len--;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
